c1ba2a7cee8ac14b7e31b8420f78087f3c2df3e4
Scott Fox <scott.fox34@gmail.com>
2015-08-05 06:47:24 -0400

    Add overloaded methods.  Check if destination directory exists.

--- EnumerableExtensions.cs
+++ EnumerableExtensions.cs
@@ -1,4 +1,5 @@
-﻿using ClosedXML.Excel;
+﻿using System.IO;
+using ClosedXML.Excel;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,18 +9,21 @@ namespace FoxOpenXML
     public static class EnumerableExtensions
     {
         /// <summary>
-        /// Convert list of delimited items to xls.
+        /// Convert list of delimited items to Excel.
         /// </summary>
         /// <typeparam name="T"></typeparam>
         /// <param name="source">Delimited list of items.</param>
         /// <param name="filePath">Full path and file name for workbook.</param>
-        /// <param name="delimiter">Delimiter used for list item. Defaults to tab delimiter. </param>
+        /// <param name="delimiter">Delimiter used for list item. </param>
         /// <param name="worksheetName">Default name for the worksheet.</param>
-        public static void ToExcel<T>(this IEnumerable<T> source, string filePath, char delimiter = '\t', string worksheetName = "sheet1")
+        public static void ToExcel<T>(this IEnumerable<T> source, string filePath, char delimiter, string worksheetName)
         {
             var enumerable = source as IList<T> ?? source.ToList();
             if (!enumerable.Any())
                 throw new Exception("No data was detected.");
+
+            var directory = Path.GetDirectoryName(filePath);
+            if (directory != null && !Directory.Exists(directory)) Directory.CreateDirectory(directory);
 
             var workbook = new XLWorkbook();
 
@@ -38,6 +42,41 @@ namespace FoxOpenXML
             
             workbook.Worksheets.Add(dt, worksheetName);
             workbook.SaveAs(filePath);
+        }
+
+        /// <summary>
+        /// Convert list of delimited items to Excel.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source">Delimited list of items.</param>
+        /// <param name="filePath">Full path and file name for workbook.</param>
+        /// <param name="delimiter">Delimiter used for list item. </param>
+        public static void ToExcel<T>(this IEnumerable<T> source, string filePath, char delimiter)
+        {
+            source.ToExcel(filePath, delimiter, "Sheet1");
+        }
+
+        /// <summary>
+        /// Convert list of tab delimited items to Excel.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source">Delimited list of items.</param>
+        /// <param name="filePath">Full path and file name for workbook.</param>
+        /// <param name="worksheetName">Default name for the worksheet.</param>
+        public static void ToExcel<T>(this IEnumerable<T> source, string filePath, string worksheetName)
+        {
+            source.ToExcel(filePath, '\t', worksheetName);
+        }
+
+        /// <summary>
+        /// Convert list of tab delimited items to Excel.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source">Delimited list of items.</param>
+        /// <param name="filePath">Full path and file name for workbook.</param>
+        public static void ToExcel<T>(this IEnumerable<T> source, string filePath)
+        {
+            source.ToExcel(filePath, '\t', "Sheet1");
         }
     }
 }
