d7998387bef233240c873867e56fc9cab4956612
Scott Fox <scott.fox34@gmail.com>
2015-08-13 13:51:03 -0400

    Add ability to insert data into existing worksheet.

---
+++ ListExtensions.cs
@@ -0,0 +1,93 @@
+﻿using System.Linq;
+using ClosedXML.Excel;
+using System;
+using System.Collections.Generic;
+using System.Data;
+
+namespace FoxOpenXML
+{
+    public static class ListExtensions
+    {
+        /// <summary>
+        /// Create and return an XLWorkbook object.
+        /// </summary>
+        /// <param name="source">List of tuples. Tuple.Item1 = datatable (data to write to excel). Tuple.Item2 = string (workbook name).</param>
+        /// <returns>ClosedXML.XLWorkBook</returns>
+        public static XLWorkbook XlWorkbook(this IList<Tuple<DataTable, string>> source)
+        {
+            var workbook = new XLWorkbook();
+            foreach (var t in source)
+            {
+                workbook.Worksheets.Add(t.Item1, t.Item2);
+            }
+            return workbook;
+        }
+
+        /// <summary>
+        /// Returns a datatable created from an IList of items.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source">Delimited list of items.</param>
+        /// <param name="delimiter">Delimiter used for list item.</param>
+        /// <returns>Datatable</returns>
+        public static DataTable CreateDataTable<T>(this IList<T> source, char delimiter)
+        {
+            var dt = new DataTable();
+            var columns = source[0].ToString().Split(delimiter).Count();
+            for (var ii = 0; ii < columns; ii++)
+            {
+                dt.Columns.Add();
+            }
+
+            foreach (var objSplit in source.Select(item => item.ToString().Split(delimiter)).Where(objSplit => objSplit != null))
+            {
+                var cols = objSplit.Count();
+                if (cols > columns)
+                {
+                    var diff = cols - columns;
+                    for (var ii = 0; ii < diff; ii++)
+                    {
+                        dt.Columns.Add();
+                    }
+                    columns = cols;
+                }
+
+                // ReSharper disable once CoVariantArrayConversion
+                dt.Rows.Add(objSplit);
+            }
+            return dt;
+        }
+
+        /// <summary>
+        /// Convert list of delimited items to Excel.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source">IList of IEnumerable delimited items. Each list of IEnumerable will be put into a separate spreadsheet tab.</param>
+        /// <param name="filePath">Full path and file name for workbook.</param>
+        /// <param name="delimiter">Delimiter used for list item.</param>
+        /// <param name="worksheetNames">Default names for the worksheet, with each name going into a unique tab. If null it will default to "sheet1", "sheet2", etc.</param>
+        public static void ToExcel<T>(this IList<IEnumerable<T>> source, string filePath, char delimiter, List<string> worksheetNames = null)
+        {
+            var dtTupleList = new List<Tuple<DataTable, string>>();
+            var index = 0;
+            foreach (var s in source)
+            {
+                var enumerable = s as IList<T> ?? s.ToList();
+
+                var name = String.Format("Sheet{0}{1}", index, 1);
+                if (worksheetNames != null && worksheetNames.Any() && worksheetNames.Count >= index + 1)
+                {
+                    name = worksheetNames[index];
+                }
+
+                var tuple = new Tuple<DataTable, string>(enumerable.CreateDataTable(delimiter), name);
+                dtTupleList.Add(tuple);
+
+                index++;
+            }
+
+            var workbook = dtTupleList.XlWorkbook();
+            workbook.SaveAs(filePath);
+        }
+    }
+}
---
+++ XLWorkbookExtensions.cs
@@ -0,0 +1,42 @@
+﻿using ClosedXML.Excel;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+
+namespace FoxOpenXML
+{
+    public static class XlWorkbookExtensions
+    {
+        /// <summary>
+        /// Import data into an existing XLWorkbook worksheet. Currently imports data starting at cell 1,1.
+        /// </summary>
+        /// <param name="source">Existing XLWorkbook.</param>
+        /// <param name="data">Data to import.  Delimited IEnumerable of strings.</param>
+        /// <param name="delimiter">Delimiter to use.</param>
+        /// <param name="worksheetName">Worksheet name to import into.  Creates a new sheet if it does not currently exist.</param>
+        /// <returns>XLWorkbook (unsaved to disk).</returns>
+        public static XLWorkbook InsertData(this XLWorkbook source, IEnumerable<string> data, string worksheetName, char delimiter = '\t')
+        {
+            if (source == null) throw new NullReferenceException("XLWorkbook cannot be null.");
+            if (data == null) throw new NullReferenceException("IEnumerable cannot be null.");
+            if (string.IsNullOrEmpty(worksheetName)) throw new NullReferenceException("worksheetName cannot be null.");
+
+            IXLWorksheet worksheet;
+            var isWorksheetPresent = source.TryGetWorksheet(worksheetName, out worksheet);
+            if (!isWorksheetPresent)
+            {
+                source.Worksheets.Add(worksheetName);
+                isWorksheetPresent = source.TryGetWorksheet(worksheetName, out worksheet);
+                if (!isWorksheetPresent) throw new Exception("Specified worksheet is not present and could not be added.");
+            }
+
+            if (worksheet == null) throw new Exception("Unknown error occurred retrieving specified worksheet.");
+
+            var dt = data.ToList().CreateDataTable(delimiter);
+            worksheet.Cell(1, 1).InsertData(dt.AsEnumerable());
+
+            return source;
+        }
+    }
+}
